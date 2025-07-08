f8ab45c7ee573277319f32415cdd7de0e982678e
Nikita Karaev <nik@pixelwise.ai>
2025-07-08 13:34:30 +0000

    fix readme

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Download the example data using gdown
-gdown --id 1q6n2R5ihfMoD-dU_u5vfcMALZSihNgiq -O ./assets/example0/snowboard.npz
+mkdir -p ./assets/example1
+gdown --id 1q6n2R5ihfMoD-dU_u5vfcMALZSihNgiq -O ./assets/example1/snowboard.npz
